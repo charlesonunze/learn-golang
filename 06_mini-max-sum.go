c1578211bb527fc91ad50705157abb17c465a32d
charlesonunze <charlesonunze2@gmail.com>
2020-09-04 15:58:10 +0100

    mini max sum

---
+++ 06_mini-max-sum.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func miniMaxSum(arr []int) {
+	var _arr []int
+	var i, j int
+
+	arrayLength := int(len(arr))
+
+	for i = 0; i < arrayLength; i++ {
+		var min, max int
+		for j = 0; j < arrayLength; j++ {
+			if i != j {
+				min += arr[j]
+				max += arr[j]
+			}
+		}
+		_arr = append(_arr, min)
+	}
+
+	sort.Ints(_arr)
+	fmt.Println(_arr[0], _arr[arrayLength-1])
+}
+
+func main() {
+	miniMaxSum([]int{1, 3, 5, 7, 9})
+}
